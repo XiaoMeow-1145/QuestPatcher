256d7fe13e21167e81e6edd8eb1c8ff809b4d55b
Xiao_Meow_CN <88760256+XiaoMeow-1145@users.noreply.github.com>
2026-06-09 18:48:49 +0800

    Fix typo in device ID check for iqiyi

--- QuestPatcher.Core/AndroidDebugBridge.cs
+++ QuestPatcher.Core/AndroidDebugBridge.cs
@@ -269,7 +269,7 @@ namespace QuestPatcher.Core
             return (await ListDevices()).OrderBy(device =>
                 device.Id.Contains("quest", StringComparison.OrdinalIgnoreCase) || 
                 device.Id.Contains("pico", StringComparison.OrdinalIgnoreCase) || 
-                device.Id.Contains("iqyiy", StringComparison.OrdinalIgnoreCase) || 
+                device.Id.Contains("iqiyi", StringComparison.OrdinalIgnoreCase) || 
                 device.Id.Contains("yvr", StringComparison.OrdinalIgnoreCase) ? 1 : 0)
                 .ToList();
         }
